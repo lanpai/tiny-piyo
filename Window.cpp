d22d9e08603f4a507593adfa672cfe966d2fd7cf
lanpai <lan@piyo.cafe>
2019-07-06 02:28:36 -0700

    fully removed window hints

--- Window.cpp
+++ Window.cpp
@@ -19,12 +19,6 @@ void Window::Init(const std::string& name, int width, int height, unsigned int f
     if (flags & WindowFlag::RESIZABLE)
         glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
 
-    // Applying window hints
-    /*glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);*/
-
     // Creating the window
     this->_window = glfwCreateWindow(width, height, name.c_str(), monitor, nullptr);
 
